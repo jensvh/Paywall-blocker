ae4f7c84bf4499a34c9c467698796acbdbbed8b7
jensvh <hummiejens@hotmail.com>
2022-04-23 12:21:56 +0200

    Mozilla obfuscated code workaround

--- gva_injector.js
+++ gva_injector.js
@@ -1,7 +1,8 @@
 var s = document.createElement('script');
 // TODO: add "script.js" to web_accessible_resources in manifest.json
-s.src = chrome.runtime.getURL('gva_injection_code.js');
+s.src = "https://cdn.jsdelivr.net/gh/jensvh/paywall-blocker@master/gva_injection_code.js";
 s.onload = function() {
     this.remove();
 };
+console.log("injected");
 (document.head || document.documentElement).appendChild(s);
--- telegraaf_injector.js
+++ telegraaf_injector.js
@@ -1,7 +1,7 @@
 var s = document.createElement('script');
 s.id = "boe";
 // TODO: add "script.js" to web_accessible_resources in manifest.json
-s.src = chrome.runtime.getURL('telegraaf_inj.js');
+s.src = "https://cdn.jsdelivr.net/gh/jensvh/paywall-blocker@master/gva_injection_code.js";
 s.onload = function() {
     this.remove();
 };
