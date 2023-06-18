9dd3bf5d7dc814c7a39c8a3a4a58e65f6e600bc4
jevhumbe <jens.vanhumbeeck@ugent.be>
2023-06-18 20:52:53 +0200

    Convert manifest to V3
    Converted using: https://github.com/GoogleChromeLabs/extension-manifest-converter

--- knack_inject_code.js
+++
@@ -1,7 +0,0 @@
-// Hide addblocker and blury overlay
-var sheet = document.createElement("style");
-window.document.head.appendChild(sheet);
-sheet.sheet.insertRule('.bcToaster, #adBlockerPopup, #paywall-modal, .rmgPaywall, .article-body p:first-of-type, .OUTBRAIN { display: none!important; }');
-sheet.sheet.insertRule('.rmgDetail-main.m-blurred * {text-shadow: none!important; color: inherit!important;}');
-sheet.sheet.insertRule('.article-body div:first-of-type {display: block!important;}');
-sheet.sheet.insertRule('body {overflow: auto!important}');
--- telegraaf_injector.js
+++
@@ -1,9 +0,0 @@
-/*var s = document.createElement('script');
-s.id = "boe";
-// TODO: add "script.js" to web_accessible_resources in manifest.json
-s.src = "https://cdn.jsdelivr.net/gh/jensvh/paywall-blocker@master/obfcode/gva_injection_code.js";
-s.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(s);*/
-// TODO
